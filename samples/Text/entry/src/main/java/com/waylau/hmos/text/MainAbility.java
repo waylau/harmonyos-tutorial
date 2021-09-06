64552c0ae1284d993fae44ba1279af6e73296a40
waylau <waylau521@gmail.com>
2021-09-06 23:54:00 +0800

    feat: update to SDK V5

--- samples/Text/entry/src/main/java/com/waylau/hmos/text/MainAbility.java
+++ samples/Text/entry/src/main/java/com/waylau/hmos/text/MainAbility.java
@@ -1,6 +1,5 @@
 package com.waylau.hmos.text;
 
-import com.waylau.hmos.text.slice.AutoFontSizeAbilitySlice;
 import com.waylau.hmos.text.slice.MainAbilitySlice;
 import ohos.aafwk.ability.Ability;
 import ohos.aafwk.content.Intent;
