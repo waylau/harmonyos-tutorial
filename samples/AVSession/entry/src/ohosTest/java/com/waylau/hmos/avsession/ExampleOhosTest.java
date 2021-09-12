4ed1667b7b1ff2f191a2583f0e5a5bc3288b806c
waylau <waylau521@gmail.com>
2021-09-12 20:10:34 +0800

    feat: update to SDK V5

---
+++ samples/AVSession/entry/src/ohosTest/java/com/waylau/hmos/avsession/ExampleOhosTest.java
@@ -0,0 +1,14 @@
+package com.waylau.hmos.avsession;
+
+import ohos.aafwk.ability.delegation.AbilityDelegatorRegistry;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleOhosTest {
+    @Test
+    public void testBundleName() {
+        final String actualBundleName = AbilityDelegatorRegistry.getArguments().getTestBundleName();
+        assertEquals("com.waylau.hmos.avsession", actualBundleName);
+    }
+}
