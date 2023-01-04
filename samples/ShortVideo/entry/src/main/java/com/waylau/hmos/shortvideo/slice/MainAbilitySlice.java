c0ed4a68a5cd2c2a6d5675d5f1694e2df0dbf70d
waylau <waylau521@gmail.com>
2023-01-05 07:54:45 +0800

    init ShortVideo demo

---
+++ samples/ShortVideo/entry/src/main/java/com/waylau/hmos/shortvideo/slice/MainAbilitySlice.java
@@ -0,0 +1,23 @@
+package com.waylau.hmos.shortvideo.slice;
+
+import com.waylau.hmos.shortvideo.ResourceTable;
+import ohos.aafwk.ability.AbilitySlice;
+import ohos.aafwk.content.Intent;
+
+public class MainAbilitySlice extends AbilitySlice {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setUIContent(ResourceTable.Layout_ability_main);
+    }
+
+    @Override
+    public void onActive() {
+        super.onActive();
+    }
+
+    @Override
+    public void onForeground(Intent intent) {
+        super.onForeground(intent);
+    }
+}
---
+++ samples/ShortVideo/entry/src/test/java/com/waylau/hmos/shortvideo/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.waylau.hmos.shortvideo;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
