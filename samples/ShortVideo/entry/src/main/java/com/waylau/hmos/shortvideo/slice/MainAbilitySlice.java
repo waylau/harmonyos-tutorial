b0ac0e011bcb5e901b19fa9af61ac01f098f35f2
waylau <waylau521@gmail.com>
2023-01-10 16:33:14 +0800

    complete 导航栏

--- samples/ShortVideo/entry/src/main/java/com/waylau/hmos/shortvideo/slice/MainAbilitySlice.java
+++ samples/ShortVideo/entry/src/main/java/com/waylau/hmos/shortvideo/slice/MainAbilitySlice.java
@@ -3,12 +3,33 @@ package com.waylau.hmos.shortvideo.slice;
 import com.waylau.hmos.shortvideo.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
+import ohos.agp.components.TabList;
 
 public class MainAbilitySlice extends AbilitySlice {
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_main);
+
+        // 初始化UI组件
+        initUi();
+    }
+
+    private void initUi() {
+        // 初始化TabList标签栏
+        TabList tabList = (TabList) findComponentById(ResourceTable.Id_tab_list);
+        TabList.Tab tab = tabList.new Tab(getContext());
+        tab.setText("首页");
+        tabList.addTab(tab);
+        TabList.Tab tab2 = tabList.new Tab(getContext());
+        tab2.setText("✚");
+        tabList.addTab(tab2);
+        TabList.Tab tab3 = tabList.new Tab(getContext());
+        tab3.setText("我");
+        tabList.addTab(tab3);
+
+        // 各个Tab的宽度也会根据TabList的宽度而平均分配
+        tabList.setFixedMode(true);
     }
 
     @Override
